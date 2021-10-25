09a98b7851e41af5a3d050a4c4c42a67d0cc697a
jose-g-cortez-m <josecortez1991_@hotmail.com>
2021-10-25 11:31:04 -0500

    Todos los reportes y fechas restricciones

--- src/Controller/Counter.php
+++ src/Controller/Counter.php
@@ -3,15 +3,20 @@
 namespace App\Controller;
 
 
+use Dompdf\Dompdf;
 use App\Entity\Project;
-use App\Form\FilterProjectsCountType;
-use App\Repository\ProjectCloseRepository;
+use App\Repository\TaskRepository;
 use App\Repository\ToolRepository;
+use App\Repository\CableRepository;
+use App\Repository\ClientRepository;
+use App\Form\FilterProjectsCountType;
+use App\Repository\ProjectRepository;
+use App\Repository\MaterialRepository;
+use App\Repository\ProjectCloseRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Dompdf\Dompdf;
 
 
 /**
@@ -61,20 +66,20 @@ class Counter extends abstractController
     }
 
     /**
-     * @Route ("/report", name="report", methods={"GET"} )
+     * @Route ("/report_tool", name="report_tool", methods={"GET"} )
      */
-    public function dataDownload(
+    public function reportTool(
         Request $request,
         ToolRepository $toolRepository
         )
     {
-      // On définitions les options du PDF
+    // Definimos las opciones de PDF
    
       /*$pdfOptions = new Options();
    
       $pdfOptions->set('defaultFont', 'Arial'));*/
-   
-      // On instancie Dompdf
+
+    // Instalamos Dompdf
    
       $dompdf = new Dompdf();
    
@@ -88,10 +93,10 @@ class Counter extends abstractController
    
       $dompdf->setHttpContext($content);
    
-      // On génère le html
+      // Generamos el html
       $tools = $toolRepository->findAll();
    
-      $html = $this->renderView('counter/report.html.twig', [
+      $html = $this->renderView('counter/reportTool.html.twig', [
         'tools' => $tools 
      ]);
    
@@ -99,11 +104,11 @@ class Counter extends abstractController
       $dompdf->setPaper('A4', 'portrait');
       $dompdf->render();
    
-      // On génère un nom de ficher
+      // Generamos el html
    
       $file = 'user-data-' . $this->getUser()->getId() . '.pdf';
    
-      // On envoie le PDF au navigateur
+    // Enviamos el PDF al navegador
    
       $dompdf->stream($file, [
         'Attachement' => true
@@ -111,5 +116,267 @@ class Counter extends abstractController
    
       return new Response();
     }
+
+    /**
+     * @Route ("/report_material", name="report_material", methods={"GET"} )
+     */
+    public function reportMaterial(
+      Request $request,
+      MaterialRepository $materialRepository
+      )
+  {
+  // Definimos las opciones de PDF
+ 
+    /*$pdfOptions = new Options();
+ 
+    $pdfOptions->set('defaultFont', 'Arial'));*/
+
+  // Instalamos Dompdf
+ 
+    $dompdf = new Dompdf();
+ 
+    $content = stream_context_create([
+      'ssl' => [
+        'verify_peer' => FALSE,
+        'verify_peer_name' => FALSE,
+        'allow_self_signed' => TRUE,
+      ]
+    ]);
+ 
+    $dompdf->setHttpContext($content);
+ 
+    // Generamos el html
+    $material = $materialRepository->findAll();
+ 
+    $html = $this->renderView('counter/reportMaterial.html.twig', [
+      'materials' => $material 
+   ]);
+ 
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper('A4', 'portrait');
+    $dompdf->render();
+ 
+    // Generamos el html
+ 
+    $file = 'user-data-' . $this->getUser()->getId() . '.pdf';
+ 
+  // Enviamos el PDF al navegador
+ 
+    $dompdf->stream($file, [
+      'Attachement' => true
+    ]);
+ 
+    return new Response();
+  }
+    /**
+     * @Route ("/report_cable", name="report_cable", methods={"GET"} )
+     */
+    public function reportCable(
+      Request $request,
+      CableRepository $cableRepository
+      )
+  {
+  // Definimos las opciones de PDF
+ 
+    /*$pdfOptions = new Options();
+ 
+    $pdfOptions->set('defaultFont', 'Arial'));*/
+
+  // Instalamos Dompdf
+ 
+    $dompdf = new Dompdf();
+ 
+    $content = stream_context_create([
+      'ssl' => [
+        'verify_peer' => FALSE,
+        'verify_peer_name' => FALSE,
+        'allow_self_signed' => TRUE,
+      ]
+    ]);
+ 
+    $dompdf->setHttpContext($content);
+ 
+    // Generamos el html
+    $cable = $cableRepository->findAll();
+ 
+    $html = $this->renderView('counter/reportCable.html.twig', [
+      'cables' => $cable 
+   ]);
+ 
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper('A4', 'portrait');
+    $dompdf->render();
+ 
+    // Generamoscable
+ 
+    $file = 'user-data-' . $this->getUser()->getId() . '.pdf';
+ 
+  // Enviamos el PDF al navegador
+ 
+    $dompdf->stream($file, [
+      'Attachement' => true
+    ]);
+ 
+    return new Response();
+  }
+
+    /**
+     * @Route ("/report_client", name="report_client", methods={"GET"} )
+     */
+    public function reportClient(
+      Request $request,
+      ClientRepository $clientRepository
+      )
+  {
+  // Definimos las opciones de PDF
+ 
+    /*$pdfOptions = new Options();
+ 
+    $pdfOptions->set('defaultFont', 'Arial'));*/
+
+  // Instalamos Dompdf
+ 
+    $dompdf = new Dompdf();
+ 
+    $content = stream_context_create([
+      'ssl' => [
+        'verify_peer' => FALSE,
+        'verify_peer_name' => FALSE,
+        'allow_self_signed' => TRUE,
+      ]
+    ]);
+ 
+    $dompdf->setHttpContext($content);
+ 
+    // Generamos el html
+    $clients = $clientRepository->findAll();
+ 
+    $html = $this->renderView('counter/reportClient.html.twig', [
+      'clients' => $clients 
+   ]);
+ 
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper('A4', 'portrait');
+    $dompdf->render();
+ 
+    // Generamos el html
+ 
+    $file = 'user-data-' . $this->getUser()->getId() . '.pdf';
+ 
+  // Enviamos el PDF al navegador
+ 
+    $dompdf->stream($file, [
+      'Attachement' => true
+    ]);
+ 
+    return new Response();
+  }
+
+  /**
+     * @Route ("/report_task", name="report_task", methods={"GET"} )
+     */
+    public function reportTask(
+      Request $request,
+      TaskRepository $taskRepository
+      )
+  {
+  // Definimos las opciones de PDF
+ 
+    /*$pdfOptions = new Options();
+ 
+    $pdfOptions->set('defaultFont', 'Arial'));*/
+
+  // Instalamos Dompdf
+ 
+    $dompdf = new Dompdf();
+ 
+    $content = stream_context_create([
+      'ssl' => [
+        'verify_peer' => FALSE,
+        'verify_peer_name' => FALSE,
+        'allow_self_signed' => TRUE,
+      ]
+    ]);
+ 
+    $dompdf->setHttpContext($content);
+ 
+    // Generamos el html
+    $tasks = $taskRepository->findAll();
+ 
+    $html = $this->renderView('counter/reportTask.html.twig', [
+      'tasks' => $tasks 
+   ]);
+ 
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper('A4', 'portrait');
+    $dompdf->render();
+ 
+    // Generamos el html
+ 
+    $file = 'user-data-' . $this->getUser()->getId() . '.pdf';
+ 
+  // Enviamos el PDF al navegador
+ 
+    $dompdf->stream($file, [
+      'Attachement' => true
+    ]);
+ 
+    return new Response();
+  }
+
+
+
+  /**
+     * @Route ("/report_project", name="report_project", methods={"GET"} )
+     */
+    public function reportProject(
+      Request $request,
+      ProjectRepository $projectRepository
+      )
+  {
+  // Definimos las opciones de PDF
+ 
+    /*$pdfOptions = new Options();
+ 
+    $pdfOptions->set('defaultFont', 'Arial'));*/
+
+  // Instalamos Dompdf
+ 
+    $dompdf = new Dompdf();
+ 
+    $content = stream_context_create([
+      'ssl' => [
+        'verify_peer' => FALSE,
+        'verify_peer_name' => FALSE,
+        'allow_self_signed' => TRUE,
+      ]
+    ]);
+ 
+    $dompdf->setHttpContext($content);
+ 
+    // Generamos el html
+    $projects = $projectRepository->findAll();
+ 
+    $html = $this->renderView('counter/reportProject.html.twig', [
+      'projects' => $projects 
+   ]);
+ 
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper('A4', 'portrait');
+    $dompdf->render();
+ 
+    // Generamos el html
+ 
+    $file = 'user-data-' . $this->getUser()->getId() . '.pdf';
+ 
+  // Enviamos el PDF al navegador
+ 
+    $dompdf->stream($file, [
+      'Attachement' => true
+    ]);
+ 
+    return new Response();
+  }
+
 
 }
